bd9966584c8914c8fe870621472edc23dd68f662
Julija777 <s.julija@icloud.com>
2022-01-18 16:28:38 +0000

    trying to male a plan for password credentials or token thing

---
+++ routes/password/passwordCredentials.js
@@ -0,0 +1,27 @@
+//Plan
+
+/* 1. Create a new table in PostgraSQL (or use existing one if created) 
+where
+Passwords going to be kept.*/
+
+/*Example of credentials :
+
+var cred = new PasswordCredential({
+  id: id,
+  password: password,
+  name: name,
+  iconURL: iconUrl
+});
+
+navigator.credentials.store(cred)
+ .then(function() {
+ // Do something else.
+});
+*/
+
+/* 
+Or maybe better to try this token things method?
+https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
+
+
+*/
