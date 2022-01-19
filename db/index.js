16c83d8e6abc7ed6cdc340a74973259a7ae46372
21ciprian <2021ciprianoffice@gmail.com>
2022-01-19 14:54:37 +0000

    Added netlify url to app.use(cors())

--- db/index.js
+++ db/index.js
@@ -2,11 +2,6 @@ import pg from 'pg'
 import {dbConfig} from '../config.js'
 const pool = new pg.Pool({
 	connectionString: dbConfig.db_url,
-	// user: dbConfig.user,
-	// host: dbConfig.host,
-	// database: dbConfig.database,
-	// password: dbConfig.password,
-	// port: dbConfig.port,
 	ssl: {rejectUnauthorized: false},
 })
 export default async function query(text, params) {
@@ -14,3 +9,9 @@ export default async function query(text, params) {
 	console.log('This is the query: ', res)
 	return res
 }
+
+// user: dbConfig.user,
+// host: dbConfig.host,
+// database: dbConfig.database,
+// password: dbConfig.password,
+// port: dbConfig.port,
